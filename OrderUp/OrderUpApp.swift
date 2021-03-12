eb329dae46d5a6f9f05cc870821c11879aa316d8
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-03-12 12:06:50 -0600

    Initial Commit

---
+++ OrderUp/OrderUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderUpApp.swift
+//  OrderUp
+//
+//  Created by Desiree on 3/12/21.
+//
+
+import SwiftUI
+
+@main
+struct OrderUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
